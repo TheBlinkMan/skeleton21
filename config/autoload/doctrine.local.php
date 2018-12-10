059f442ff777cc09b05c1315a144c95e6c57feba
TheBlinkMan <cardosoalanf@gmail.com>
2018-12-09 22:24:57 -0200

    Add annotation driver to doctrine local config

--- config/autoload/doctrine.local.php
+++ config/autoload/doctrine.local.php
@@ -14,6 +14,22 @@ return [
                     'charset'  => 'utf8',
                 ],
             ],
+          ],
+        'driver' => [
+            'my_annotation_driver' => [
+                'class' => \Doctrine\ORM\Mapping\Driver\AnnotationDriver::class,
+                'cache' => 'array',
+                'paths' => [
+                    'module/Application/src/Application/Entity',
+                ],
+
+            ],
+            'orm_default' => [
+                'drivers' => [
+                    // register `my_annotation_driver` for any entity under namespace `My\Namespace`
+                    'Application\Entity' => 'my_annotation_driver',
+                ],
+            ],
         ],
-    ],
+   ],
 ];
